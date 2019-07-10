794a9387fc12914d7d88248f15cd09c68a2340b4
Sandeep Desu <Sandeep.Desu2@T-Mobile.com>
2019-07-10 16:10:46 +0530

    updated port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const mongoose = require('mongoose');
-const server = new Hapi.Server({ "host": "localhost", "port": 3000 });
+const server = new Hapi.Server({ "host": "localhost", "port": 4000 });
 mongoose.connect("mongodb://localhost:27017/sample");
 const routes = require('./routes');
 server.route(routes);
