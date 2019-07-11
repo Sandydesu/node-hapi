9bd0e7e205551a8caffacdfbcece1d5eeedcb498
Sandeep Desu <Sandeep.Desu2@T-Mobile.com>
2019-07-11 15:18:57 +0530

    updated with cros origin option

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const Hapi = require('@hapi/hapi');
 const mongoose = require('mongoose');
-const server = new Hapi.Server({ "host": "localhost", "port": 4000 });
+const server = new Hapi.Server({
+    "host": "localhost",
+    "port": 4000,
+    "routes": {
+        "cors": {
+            "origin": ["*"],
+            "headers": ["Accept", "Content-Type"], //accepting headers
+            "additionalHeaders": ["X-Requested-With"] //additional headers
+        }
+    }
+});
 mongoose.connect("mongodb://localhost:27017/sample");
 const routes = require('./routes');
 server.route(routes);
